783cfb5e690b63a42d0016305dd5bc2eaff84cfd
momofor <mohammedclacher2014@gmail.com>
2020-11-09 20:43:40 +0100

    organized config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,11 +12,28 @@
 # if status --is-interactive
 #   ...
 # end
+##################################START OF AUTOSTART ###############
 # "autostart"
 function fish_greeting
 test -r "~/.dir_colors" && eval (dircolors ~/.dir_colors)
 colorscript -e colortest
 end
+#################################END OF AUTOSTART########################
 
-#nord dir_colors
+################START OF EXPORTS##################################
+#exports
+set -x EDITOR nvim
+###############END OF EXPORTS############################################
 
+##################START OF ABREVIATIONS##################################
+
+SETUVAR _fish_abbr__2E_2E_:cd\x20\x2e\x2e   
+SETUVAR _fish_abbr_cl:clear
+SETUVAR _fish_abbr_ct:colorscript\x20\x2de\x20colortest
+SETUVAR _fish_abbr_g:git
+SETUVAR _fish_abbr_gs:git\x20status
+SETUVAR _fish_abbr_ht:htop
+SETUVAR _fish_abbr_l:ls\x20\x2dcolor\x20\x2dall
+SETUVAR _fish_abbr_n:nvim
+SETUVAR _fish_abbr_neo:neofetch
+####################END OF ABREVIATIONS##################################
