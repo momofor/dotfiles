cb21bd672cd376c9bf4dbfec3b83cd623eed021f
momofor <mohammedclacher2014@gmail.com>
2020-11-09 16:50:19 +0100

    added fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,22 @@
+# Put system-wide fish configuration entries here
+# or in .fish files in conf.d/
+# Files in conf.d can be overridden by the user
+# by files with the same name in $XDG_CONFIG_HOME/fish/conf.d
+
+# This file is run by all fish instances.
+# To include configuration only for login shells, use
+# if status --is-login
+#    ...
+# end
+# To include configuration only for interactive shells, use
+# if status --is-interactive
+#   ...
+# end
+# "autostart"
+function fish_greeting
+test -r "~/.dir_colors" && eval (dircolors ~/.dir_colors)
+echo its working 
+end
+
+#nord dir_colors
+
---
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/momofor/.local/share/omf/themes/sashimi/fish_prompt.fish
