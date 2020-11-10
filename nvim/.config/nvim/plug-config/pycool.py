89e87871b4931dd77d29a49d505eb6f28b95bdb6
momofor <mohammedclacher2014@gmail.com>
2020-11-10 12:50:45 +0100

    cleaned this branch

--- nvim/.config/nvim/plug-config/pycool.py
+++
@@ -1,7 +0,0 @@
-class student :
-    def __init__(self,name,age,grade):
-        self.name = name
-        self.grade = grade
-        self.age = age
-std1=student("nathan",19,90)
-print(std1.age)
--- nvim/.config/nvim/plug-config/python-syntax.vim
+++
@@ -1,4 +0,0 @@
-let g:python_highlight_all = 1
-let g:python_highlight_space_errors = 1
-let g:python_highlight_class_vars = 1
-
