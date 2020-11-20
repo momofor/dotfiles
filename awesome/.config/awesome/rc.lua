310bb1f0eb4c803c662fbe3a627f3d5fb896632f
momofor <mohammedclacher2014@gmail.com>
2020-11-20 18:28:22 +0100

    floating mode

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -101,7 +101,7 @@ awful.util.terminal = terminal
 awful.util.tagnames = { "1", "2", "3", "4", "5" }
 awful.layout.layouts = {
     awful.layout.suit.tile,
-    --awful.layout.suit.floating,
+    awful.layout.suit.floating,
     --awful.layout.suit.tile.left,
     --awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
--- nvim/.config/nvim/plug-config/bracey.vim
+++ nvim/.config/nvim/plug-config/bracey.vim
@@ -1,3 +1,2 @@
 let g:bracey_server_allow_remote_connections=1
 let g:bracey_auto_start_server=1
-g:bracey_server_pornt='http://momofor-website'
