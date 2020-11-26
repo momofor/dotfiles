b820f975734e1a2c5a5b6340f7087a1664c0ca55
momofor <mohammedclacher2014@gmail.com>
2020-11-26 17:54:34 +0100

    changed font to 12

--- nvim/.config/nvim/demos/demo.py
+++ nvim/.config/nvim/demos/demo.py
@@ -2,6 +2,7 @@ class student:
     def __init__(self,name,class1):
         self.name = name
         self.class1 = class1
-
+    def get_name(self):
+        return self.name
 std1= student("momofor","3A")
 print("And his name is ",std1.name , "And he is " , std1.class1 )
