eec3aa7b85f80583de99cbfd025b4a50ecd789de
momofor <mohammedclacher2014@gmail.com>
2021-02-18 18:40:08 +0100

    added nord youtube css

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -92,6 +92,7 @@ local chosen_theme = themes[11]
 local modkey       = "Mod4"
 local altkey       = "Mod1"
 local terminal     = "kitty"
+local terminal_exec     = terminal .. " -e "
 local vi_focus     = false -- vi-like client focus - https://github.com/lcpz/awesome-copycats/issues/275
 local cycle_prev   = true -- cycle trough all previous client or just the first -- https://github.com/lcpz/awesome-copycats/issues/274
 local editor       = os.getenv("EDITOR") or "nvim"
