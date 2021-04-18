2cfab33e8c549283364d0b425cfb735bc8b6e6fb
momofor <mohammedclacher2014@gmail.com>
2021-04-18 20:51:27 +0000

    more efficient code

--- awesome/.config/awesome/my_utils/ChangeLayout.lua
+++ awesome/.config/awesome/my_utils/ChangeLayout.lua
@@ -4,14 +4,13 @@ I = 1
 
 function ChangeLayout()
     CurrentLayout = Layouts[I]
-    for index, value in ipairs (Layouts) do
-        Max = index
-    end
-    if I < Max then
+    local max = #Layouts
+    if I < max then
         I = I + 1
-    else
-        I = 1
+        else
+            I = 1
     end
+
     naughty.notify({
         title = "You Changed you layout to " .. CurrentLayout ,
         text = "To Change you layout again press mod ctl l" })
--- scripts/scripts/script.lua
+++ scripts/scripts/script.lua
@@ -1,3 +1,6 @@
+#!/usr/bin/env lua
 local _ = require("_helpers")
 
-
+print("what is your name")
+local input = _.input()
+print("hello " .. input)
