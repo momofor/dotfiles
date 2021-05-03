f1af560fef6dd54677dd4c91692c1bd22a1cc1e7
momofor <mohammedclacher2014@gmail.com>
2021-05-03 22:32:53 +0000

    made lua tui library

--- scripts/scripts/_tui.lua
+++ scripts/scripts/_tui.lua
@@ -11,30 +11,59 @@
                                                                              
                                                                              
 
-local _h = require("_helpers")
-local func = require("bootstrap_helpers")
+local _ = require("_helpers")
 
-print("Welcome to my bootstrapping script, What do you want to do ?")
-print("(C)heck what you need")
-local user_option = _h.input(nil , "command: ")
-local apps = {
-    "awesome" ,
-    "fish" ,
-    "stow" ,
-    "git" ,
-    "kitty" ,
-    "rofi" ,
-    "firefox" ,
-    "cool" ,
-    "foo" ,
-    "bar"
-}
+local ui = {Current = 1 , words = {} , functions = {}}
 
-if user_option == "C" then
-    local not_found = func.is_app_installed(apps)
-    io.write("missing apps: ")
-    for _, app in pairs(not_found) do
-        io.write(string.format("%s " , app))
+function ui:new(o ,Current , words , functions)
+    o = o or {}
+    setmetatable(o , self)
+    self.__index = self
+    self.Current = Current or 1
+    self.words = words or {}
+    self.functions = functions or {}
+    _.exec("clear")
+    ui:start()
+    while true
+        do
+            ui:CheckInput()
+        end
+        return o
+end
+
+function ui:start()
+    for _,word in pairs(self.words) do
+        if word == self.words[self.Current] then
+            print ("-----" .. word .. "-----")
+        else
+            print(word)
+        end
     end
 end
 
+function ui:CheckInput()
+    local choice = _.input()
+    if choice == "k" and self.Current == 1 then
+        self.Current = 1
+        _.exec("clear")
+        ui:start()
+    elseif choice == "j" and self.Current == #self.words then
+        self.Current = #self.words
+        _.exec("clear")
+        ui:start()
+    elseif choice == "k" and self.Current ~= 1 then
+        self.Current = self.Current - 1
+        _.exec("clear")
+        ui:start()
+    elseif choice == "j" and self.Current ~= #self.words then
+        self.Current = self.Current + 1
+        _.exec("clear")
+        ui:start()
+    elseif choice == "no" then
+        _.exec("clear")
+        self.functions[self.Current]()
+        ui:start()
+    end
+end
+
+return ui
