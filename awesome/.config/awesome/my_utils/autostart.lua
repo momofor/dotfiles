77dfa1ec632b085641c4e7c83fe3152387229177
momofor <mohammedaminechennoufi@hotmail.com>
2021-09-16 19:43:33 +0100

    fixed kitty hotkeys

--- awesome/.config/awesome/my_utils/autostart.lua
+++ awesome/.config/awesome/my_utils/autostart.lua
@@ -2,7 +2,7 @@ local awful = require("awful")
 local auto_apps = {
     "picom --experimental-backends",
     "kitty",
-    os.getenv("BROWSER") or "firefox"
+    os.getenv("BROWSER") or "luakit"
 }
 for _, app in pairs(auto_apps) do
     awful.spawn.with_shell(app)
