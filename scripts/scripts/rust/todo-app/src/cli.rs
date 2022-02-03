701aaa8ac79dc980ffdcc5a71717057bcf252b7f
momofor <mohammedaminechennoufi@hotmail.com>
2022-02-03 23:36:33 +0100

    RES BACKUP

--- scripts/scripts/rust/todo-app/src/cli.rs
+++ scripts/scripts/rust/todo-app/src/cli.rs
@@ -1,7 +1,21 @@
-pub fn get_arguments(arg_number: usize) -> Result<Vec<String>, &'static str> {
+use std::cmp;
+
+pub fn get_arguments(arg_number: usize) -> Result<Vec<String>, String> {
     let arguments: Vec<String> = std::env::args().collect();
     let arg_len = arguments.len().to_owned();
-
-    todo!()
+    match arg_len.cmp(&arg_number) {
+        cmp::Ordering::Greater => {
+            return Err(format!(
+                "TOO MANY ARGUMENTS expected {} arguments got {}",
+                arg_number, arg_len
+            ))
+        }
+        cmp::Ordering::Less => {
+            return Err(format!(
+                "NOT ENOUGH ARGUMENTS expected {} arguments got {}",
+                arg_number, arg_len
+            ))
+        }
+        cmp::Ordering::Equal => return Ok(arguments),
+    }
 }
-
