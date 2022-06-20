5ee705b70a9796cab21b35b86edd1eef9c0bb435
momofor <mohammedaminechennoufi@hotmail.com>
2022-06-20 18:38:41 +0100

    fixed firefox chrome

--- scripts/scripts/rust/wtf/src/main.rs
+++ scripts/scripts/rust/wtf/src/main.rs
@@ -1,31 +1,42 @@
-use futures::future::try_join_all;
-use reqwest::Client;
-use std::{error::Error, time::Instant};
+use tokio::sync::{mpsc, oneshot};
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let client = Client::builder().build()?;
-    // Start timing.
-    let now = Instant::now();
-    // Get values.
-    let mut gets = Vec::new();
-    // Default to few requests to be nice to jsonplaceholder.
-    for id in 1..=3 {
-        let get = get_todo(&client, id);
-        gets.push(get);
-    }
-    let results = try_join_all(gets).await?;
-    // Report time and result.
-    println!("Elapsed: {} seconds", now.elapsed().as_secs_f64());
-    println!("Result: {:#?}", results.last().unwrap());
-    Ok(())
+struct testuChan {
+    sendu: &'static str,
+    recieveu: Responder<()>,
 }
 
-// async fn get_todo(client: &Client, id: i32) -> Result<Value, Box<dyn Error>> {
-async fn get_todo(client: &Client, id: i32) -> Result<(), Box<std::error::Error>> {
-    let base = "https://jsonplaceholder.typicode.com/todos";
-    
-    let address = format!("{}/{}", base, id);
-    let result = client.get(&address).send().await?;
-    Ok(())
+type Responder<T> = oneshot::Sender<Result<T, Box<dyn std::error::Error>>>;
+
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = mpsc::channel(32);
+
+    let tx2 = tx.clone();
+    let tx3 = tx.clone();
+
+    tokio::spawn(async move {
+        let (tux, mut rux) = oneshot::channel();
+        let noice = testuChan {
+            sendu: "First handle",
+            recieveu: tux,
+        };
+
+        tx.send("Sending from first handle").await.unwrap();
+    });
+
+    tokio::spawn(async move {
+        let (tux, mut rux) = oneshot::channel();
+        tx2.send("Sending from second handle").await.unwrap();
+        tux.send("Finished Second handle").unwrap();
+    });
+
+    tokio::spawn(async move {
+        let (tux, mut rux) = oneshot::channel();
+        tx3.send("Sending from third handle").await.unwrap();
+        tux.send("Finished Third handle").unwrap();
+    });
+
+    while let Some(message) = rx.recv().await {
+        println!("GOT = {}", message);
+    }
 }
