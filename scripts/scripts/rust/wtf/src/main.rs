9aef66fa79a6ef5d4d1e7ce39dfc1955a66187f7
momofor <mohammedaminechennoufi@hotmail.com>
2022-07-09 23:18:14 +0100

    idk anymore

--- scripts/scripts/rust/wtf/src/main.rs
+++ scripts/scripts/rust/wtf/src/main.rs
@@ -1,23 +1,47 @@
-use tokio::io::{self, AsyncReadExt};
-use tokio::net::TcpListener;
+use std::fmt::{self, Display};
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+#[derive(Debug, Clone)]
+struct Vector3<A, B, C> {
+    x: A,
+    y: B,
+    z: C,
+}
 
-    loop {
-        let (mut socket, _) = listener.accept().await?;
-
-        tokio::spawn(async move {
-            let (mut rd, mut wr) = socket.split();
-
-            if io::copy(&mut rd, &mut wr).await.is_err() {
-                eprintln!("failed to copy");
-            }
-            // let mut noice = String::new();
-            // let read = rd.read_to_string(&mut noice).await.unwrap();
-
-            println!("GOT");
-        });
+impl Vector3<usize, usize, usize> {
+    fn new<A, B, C>(x: A, y: B, z: C) -> Vector3<A, B, C> {
+        Vector3 { x, y, z }
     }
+}
+
+impl Into<Vec<usize>> for Vector3<usize, usize, usize> {
+    fn into(self) -> Vec<usize> {
+        vec![self.x as usize, self.y as usize, self.z as usize]
+    }
+}
+
+trait test {
+    fn addu(a: i32, x: i32) -> i32;
+}
+
+impl test for Vector3<usize, usize, usize> {
+    fn addu(a: i32, x: i32) -> i32 {
+        a + x
+    }
+}
+
+impl Display for Vector3<usize, usize, usize> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({},{},{})", self.x, self.y, self.z)
+    }
+}
+
+fn main() {
+    let noice = Vector3::new(10, 5, 6);
+    let tes = noice.clone();
+    let cool: Vec<usize> = noice.into();
+    println!("noice is: {}", tes);
+
+    cool.iter()
+        .map(|x| println!("My value is: {}", x))
+        .collect::<Vec<_>>();
 }
