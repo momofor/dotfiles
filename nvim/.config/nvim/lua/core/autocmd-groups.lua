91c518cf0333d714719c4515324fc81a20e4d1ee
momofor <mohammedaminechennoufi@hotmail.com>
2025-06-13 01:57:27 +0100

    fixed stuff

--- nvim/.config/nvim/lua/core/autocmd-groups.lua
+++ nvim/.config/nvim/lua/core/autocmd-groups.lua
@@ -4,3 +4,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
 	end,
 })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	pattern = { "*.py", "*.c" },
+	callback = function()
+		vim.cmd [[LspStart]]
+	end,
+})
